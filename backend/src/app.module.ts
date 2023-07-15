682662467acaf524dcc9d23d1af7b79364ce8362
ChelderStark <helderdroid@gmail.com>
2023-07-14 21:33:57 -0300

    feat: :sparkles: the initial structure of the Users crude was created

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './@core/infra/database/prisma.module';
+import { UsersModule } from './app/users/users.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, UsersModule],
   controllers: [],
   providers: [],
 })
